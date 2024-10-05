79fd5dcb2879aceeb832d449f26b8bb5bb9d24a3
Holiday Internet User <holidayinternetuser@protonmail.com>
2024-10-05 22:29:43 +0200

    Update and rename torchlight.js to script.js

--- assets/js/torchlight.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('mousemove', (e) => {
-    const torchlight = document.querySelector('.torchlight');
-    torchlight.style.left = `${e.pageX - (torchlight.clientWidth / 2)}px`;
-    torchlight.style.top = `${e.pageY - (torchlight.clientHeight / 2)}px`;
-});
-
