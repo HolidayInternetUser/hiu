607d228c8fa22f23a73d1b178e10e9a893fa0d26
HolidayInternetUser <holidayinternetuser@protonmail.com>
2024-10-05 22:23:07 +0200

    Initial commit with torchlight effect

--- assets/js/torchlight.js
+++ assets/js/torchlight.js
@@ -1,6 +1,6 @@
 document.addEventListener('mousemove', (e) => {
     const torchlight = document.querySelector('.torchlight');
-    torchlight.style.left = `${e.pageX - 75}px`; // Center the light horizontally
-    torchlight.style.top = `${e.pageY - 75}px`; // Center the light vertically
+    torchlight.style.left = `${e.pageX - (torchlight.clientWidth / 2)}px`;
+    torchlight.style.top = `${e.pageY - (torchlight.clientHeight / 2)}px`;
 });
 
