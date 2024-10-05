55c98f8dc345723634df5f217ade9c8ce3c34a6f
HolidayInternetUser <holidayinternetuser@protonmail.com>
2024-10-05 22:13:29 +0200

    Add torchlight effect implementation

--- assets/js/torchlight.js
+++ assets/js/torchlight.js
@@ -1,6 +1,6 @@
-// JavaScript to make the torchlight follow the mouse
-document.addEventListener("mousemove", function(e) {
-    var torch = document.getElementById("torch");
-    torch.style.left = e.pageX + "px"; // Update the position of the torchlight
-    torch.style.top = e.pageY + "px";  // Update the position of the torchlight
+document.addEventListener('mousemove', (e) => {
+    const torchlight = document.querySelector('.torchlight');
+    torchlight.style.left = `${e.pageX - 75}px`; // Center the light horizontally
+    torchlight.style.top = `${e.pageY - 75}px`; // Center the light vertically
 });
+
